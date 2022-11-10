4959f5abfc0ee22ea8276f239e9476f9246c87ac
Federico Read <110434726+federead@users.noreply.github.com>
2022-11-10 19:48:12 -0300

    e7 + get

---
+++ scripts/scriptCRUD.js
@@ -0,0 +1,75 @@
+const SERVER_MOCK = "https://63606653af66cc87dc137d25.mockapi.io"
+const GET = "/users/"
+const POST = "/users"
+const PUT = "/users/"
+const DELETE = "/users/"
+
+const btnGet1 = document.getElementById("btnGet1");
+const results = document.getElementById("results");
+const inputGet1Id = document.getElementById("inputGet1Id");
+
+btnGet1.addEventListener("click", async () => {
+    try {
+        const response = await fetch(SERVER_MOCK + GET);
+        const data = await response.json();
+        showData(data);          
+    } catch (error) {
+        alert("Ocurrió un error");
+    }
+});
+
+
+
+function showData(data) {    
+    if (!inputGet1Id.value) {
+        for (const datos of data) {
+            results.innerHTML += `<p>Nombre: ${datos.name} <br>Apellido: ${datos.lastname} <br> ID: ${datos.id} `;            
+        }        
+    } else if(inputGet1Id.value) {
+        for (const datos of data) {
+            if (datos.id===inputGet1Id.value) {                
+                results.innerHTML += `<p>Nombre: ${datos.name} <br>Apellido: ${datos.lastname} <br> ID: ${datos.id} `;
+            }            
+        }
+    }    
+}
+
+
+
+const get = async (id = "")  => {
+    const response = await fetch(SERVER_MOCK + GET + id)
+    const data = await response.json()
+    return data
+}
+
+const post = async (data) => {
+    const response = await fetch(SERVER_MOCK + POST, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    const result = await response.json()
+    return result
+}
+
+const put = async (id, data) => {
+    const response = await fetch(SERVER_MOCK + PUT + id, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    const result = await response.json()
+    return result
+}
+
+const remove = async (id) => {
+    const response = await fetch(SERVER_MOCK + DELETE + id, {
+        method: "DELETE"
+    })
+    const result = await response.json()
+    return result
+}
